7fb296e1fe003e27c2c62dd7e9c0d807c2370713
S. Rasenack <stefanra06@gmx.de>
2024-10-10 11:25:03 +0200

    Zusätzliche Zeilen

--- JavaTest/src/Taschenrechner.java
+++ JavaTest/src/Taschenrechner.java
@@ -1,4 +1,59 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class Taschenrechner {
+    public static void main(String[] args) {
+
+        System.out.println("Enter first Number:");
+        String sNumber1 = new Scanner(System.in).nextLine();
+        int number1 = Integer.valueOf(sNumber1);
+
+        System.out.println("Enter second Number:");
+        int number2 = Integer.valueOf(new Scanner(System.in).nextLine());
+
+
+        System.out.println("Enter operand (+, -, *, /, ^)");
+        String Operator = new Scanner(System.in).nextLine();
+
+        int solution = 0;
+
+        /*
+        if (Operator.equals("+")) solution = number1 + number2; else
+        if (Operator.equals("-")) solution = number1 - number2; else
+        if (Operator.equals("*")) solution = number1 * number2; else
+        if (Operator.equals("/")) solution = number1 / number2; else {
+            System.out.println("Wrong Operand!");
+            return;
+        }
+        */
+
+        // if (solution == 0) System.out.println(("wrong Operand!");
+
+
+        switch (Operator) { // Switch ist eine Alternative zu Zeilen 19 - 26)
+            case "+" : solution = number1 + number2; break;
+            case "-" : solution = number1 - number2; break;
+            case "*" : solution = number1 * number2; break;
+            case "/" : solution = number1 / number2; break;
+            // case "^" : solution = Math.pow(number1, number2); break; // Math.pow gibt double aus, kein int
+
+            case "^" : solution = (int) Math.round(Math.pow(number1, number2)); break; // Typecasting von double zu int
+
+            default: System.out.println("Wrong Operand"); return;
+        }
+
+        int a = 5;
+        int b = 2;
+
+        boolean isBigger = a >= b;
+        boolean isLess = a < b;
+
+        if (isBigger) System.out.println("BIGGER");
+        if (isLess) System.out.println("LESS");
+
+
+
+        System.out.println("is" + " " + Integer.toString(solution));
+
+    }
 }
--- JavaTest/src/conditional_branches.java
+++ JavaTest/src/conditional_branches.java
@@ -2,18 +2,6 @@ public class conditional_branches {
 
     public static void main(String[] args) {
 
-        /*
-        String sNumber = new Scanner(System.in).nextLine();
-
-        int num = Integer.valueOf(sNumber);
-
-        if (num == 5) {
-            System.out.println("match");
-        } else {
-            System.out.println("not is not 5");
-        }
-        */
-
         int a = 5;
         int b = 10;
 
@@ -34,14 +22,6 @@ public class conditional_branches {
         } else {
             System.out.println("not a match");
         }
-
-
-        /*if(userName =="Kirill") {
-            System.out.printIn("cool");
-        } else{
-            System.out.println.("uncool");
-        }*/
-
 
     }
 }
