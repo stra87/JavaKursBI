09e27d1031628123a9d23d8f6d15a520520fe898
S. Rasenack <stefanra06@gmx.de>
2024-10-18 15:32:44 +0200

    Teil der Hausaufgaben

---
+++ JavaTest/src/Hausaufgaben_1.java
@@ -0,0 +1,17 @@
+public class Hausaufgaben {
+    public static void main(String[] args) {
+            int a = 4;
+            int b = 1;
+
+
+            if (a < 5 || a < 6) System.out.println("Das ist korrekt");
+            else System.out.println("Das ist falsch");
+            System.out.println(a == 3 ^ 2 == b);
+            System.out.println(b > 1 && a > 3);
+            System.out.println(a + b < 19 && b < 1);
+            System.out.println(3 < 5 && 3 > 5);
+            System.out.println((3 < 5 && a > 3) ^ b == 1);
+
+    }
+
+}
---
+++ JavaTest/src/Hausaufgaben_5.java
@@ -0,0 +1,3 @@
+public class Hausaufgaben_4 {
+    
+}
