9bda526eab133822ddcb34a6aafa1eb20a66d8af
S. Rasenack <stefanra06@gmx.de>
2024-10-22 09:29:18 +0200

    HA_7

---
+++ JavaTest/src/Hausaufgaben_7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class Hausaufgaben_7 {
+    public static void main(String[] args) {
+        System.out.println("Welche Zahl wählst du zwischen 0 und 1?");
+        String EingabeZahl = new Scanner(System.in).nextLine();
+        double Zahl = Double.valueOf(EingabeZahl);
+
+        double zufall = Math.random();
+
+        System.out.println(zufall);
+    }
+}
