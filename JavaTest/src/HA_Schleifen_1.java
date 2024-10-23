a38e735c2af1cdb7184619f248949a6721484b2c
S. Rasenack <stefanra06@gmx.de>
2024-10-23 16:30:03 +0200

    Update HA_Schleifen_1

--- JavaTest/src/HA_Schleifen_1.java
+++ JavaTest/src/HA_Schleifen_1.java
@@ -1,3 +1,29 @@
 public class HA_Schleifen_1 {
+    public static void main (String [] args) {
 
+//        System.out.println("Teilaufgabe 1:");
+//        for (int i1 = 0; i1 <= 10; i1++) {
+//            System.out.println(Integer.toString(i1));
+//        }
+//
+//        System.out.println("Teilaufgabe 2:");
+//        for (int i2 = -10; i2 <= 10; i2 = i2 +2) {
+//            System.out.println(Integer.toString(i2));
+//        }
+//
+//        System.out.println("Teilaufgabe 3:");
+//        for (int i3 = 0; i3 < 100; i3 = i3 + 3) {
+//            System.out.println(Integer.toString(i3));
+//        }
+
+        System.out.println("Teilaufgabe 4:");
+        for (int i4 = 1; i4 < 1000; i4 = i4 * 2) {
+            System.out.println(Integer.toString(i4));
+        }
+
+        System.out.println("Teilaufgabe 5:");
+        for (int i5 = 1; i5 < 1000; i5 = i5 * 2) {
+            System.out.println(Integer.toString(i5));
+        }
+    }
 }
