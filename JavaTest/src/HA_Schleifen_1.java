842fc2de9e3a8b61784c60854a494aca0cb14284
S. Rasenack <stefanra06@gmx.de>
2024-10-23 16:06:21 +0200

    Update HA_Verzweigungen_8

---
+++ JavaTest/src/HA_Schleifen_1.java
@@ -0,0 +1,3 @@
+public class HA_Schleifen_1 {
+
+}
--- JavaTest/src/HA_Verzweigungen_8.java
+++ JavaTest/src/HA_Verzweigungen_8.java
@@ -6,11 +6,38 @@ public class HA_Verzweigungen_8 {
 
         System.out.println("Welche Zahl wählt Spieler 1 aus 0 von 100?");
         String EingabeZahl1 = new Scanner(System.in).nextLine();
-        double Zahl1 = Double.valueOf(EingabeZahl1);
+        int Zahl1 = Integer.valueOf(EingabeZahl1);
 
         System.out.println("Welche Zahl wählt Spieler 2 aus 0 von 100?");
         String EingabeZahl2 = new Scanner(System.in).nextLine();
-        double Zahl2 = Double.valueOf(EingabeZahl2);
+        int Zahl2 = Integer.valueOf(EingabeZahl2);
+
+        int difZ1 = zufall - Zahl1;
+        int difZ2 = zufall - Zahl2;
+
+        if (difZ1 < 0) {
+            difZ1 *= -1;
+        }
+
+        if (difZ2 < 0) {
+            difZ2 *= -1;
+        }
+
+
+        if (difZ1 < difZ2) {
+            System.out.println("Zufallszahl: " + zufall);
+            System.out.println("Spieler 1 hat gewonnen.");
+        }
+        if (difZ1 > difZ2) {
+            System.out.println("Zufallszahl: " + zufall);
+            System.out.println("Spieler 2 hat gewonnen.");
+        }
+
+        if (difZ1 == difZ2) {
+            System.out.println("Zufallszahl: " + zufall);
+            System.out.println("Beide Spieler haben gewonnen.");
+        }
+
 
     }
 }
